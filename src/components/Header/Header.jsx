baf93e05c34f40d08c9a6b3628bba5d304320924
rony <ronyrnds@gmail.com>
2021-09-22 11:05:24 -0300

    criado projeto base

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = ()=>{
+    return(
+        <header>
+            <nav>
+                <h1>header</h1>
+            </nav>
+        </header>
+
+    )
+}
+
+export { Header }
