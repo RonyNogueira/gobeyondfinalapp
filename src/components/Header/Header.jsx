4812bddfb18cf265ad274b76a8efd90428506486
rony <ronyrnds@gmail.com>
2021-09-24 17:32:34 -0300

    added mobile menu itens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../../assets/img/logo-corebiz-global.svg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, } from '@fortawesome/free-solid-svg-icons'
 
 const Header = ()=>{
+    const [menuOpen, setMenuOpen] = useState(false)
+
     return(
         <header className="header">
             <nav className="header__container">
@@ -33,10 +35,27 @@ const Header = ()=>{
                         <img src={logo} className="header__container__logo" alt="logo da corebiz"/>
                     </a>
 
-                        <div className="header__mob-nav__content__menu-bars">
+                        <div onClick={()=> setMenuOpen(!menuOpen)} className="header__mob-nav__content__menu-bars">
                             <FontAwesomeIcon className="btn-open" icon={faBars} size={"lg"}/>
                         </div>
 
+                </div>
+
+                <div className={menuOpen ? 'header__mob-nav__menu active' : 'header__mob-nav__menu'}>
+                    <ul>
+                        <li>
+                            <a href="https://www.corebiz.ag/pt/about/" target='_blank'>a corebiz</a>
+                        </li>
+                        <li>
+                            <a href="https://www.corebiz.ag/pt/#framework-title" target='_blank'>serviços</a>
+                        </li>
+                        <li>
+                            <a href="https://www.corebiz.ag/pt/cases/" target='_blank'>cases</a>
+                        </li>
+                        <li>
+                            <a href="https://www.corebiz.ag/pt/contato/" target='_blank'>contato</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>
